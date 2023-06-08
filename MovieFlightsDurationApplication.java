9d7e0cb02b6c99b953e3d698f8b2cc89e7c3b3b4
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-06-08 20:12:14 +0300

    Add the logic of how to compare the MoviePairs created with the flight duration

--- MovieFlightsDurationApplication.java
+++ MovieFlightsDurationApplication.java
@@ -1,19 +1,21 @@
 import com.syntaxapplication.data.Flight;
 import com.syntaxapplication.data.Movie;
+import com.syntaxapplication.data.MoviePair;
 import com.syntaxapplication.services.DurationService;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MovieFlightsDurationApplication {
 
     public static void main(String[] args) {
-        List<Movie> dummyMovie =  Movie.createRandomMovies();
-        Flight dummyFlight = new Flight(170);
+        List<Movie> dummyMovies =  Movie.createRandomMovies();
+        Flight dummyFlight = new Flight(250);
         DurationService algorithmService = new DurationService();
-        dummyMovie.forEach(movie -> System.out.println(movie.getDuration()));
-        algorithmService.getAppropriateMovies(dummyMovie, dummyFlight);
+        List<MoviePair> listOfMoviePairs = algorithmService.getMoviePairs(dummyMovies);
+        MoviePair moviePair = algorithmService.getBestMoviePairAccordingToFlightDuration( listOfMoviePairs, dummyFlight.getDuration() - 30);
+        System.out.println( "Best Solution is " + moviePair.toString());
+        System.out.println( "Output: [" + moviePair.getMovieOneIndex() + "],[" + moviePair.getMovieTwoIndex() + "]");
 
     }
 }
--- com/syntaxapplication/services/DurationService.java
+++ com/syntaxapplication/services/DurationService.java
@@ -2,15 +2,45 @@ package com.syntaxapplication.services;
 
 import com.syntaxapplication.data.Flight;
 import com.syntaxapplication.data.Movie;
+import com.syntaxapplication.data.MoviePair;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DurationService {
 
-    public int[] getAppropriateMovies(List<Movie> movies, Flight flight) {
-        int[] moviesIndex;
-        int flightDuration = flight.getDuration();
+    public List<MoviePair> getMoviePairs(List<Movie> movies) {
+        List<MoviePair> moviePairs = new ArrayList<>();
+        int moviesLength = movies.toArray().length;
+        IntStream.range(0, moviesLength).forEach( movieOneIndex -> {
+                        IntStream.range(0, moviesLength).forEach(
+                                movieTwoIndex -> {
+                                        if (movieOneIndex !=  movieTwoIndex)
+                                        moviePairs.add(new MoviePair(movieOneIndex, movieTwoIndex, movies.get(movieOneIndex).getDuration() + movies.get(movieTwoIndex).getDuration() ));
+                                }
+                        );
+                }
 
-        return moviesIndex;
+        );
+        return moviePairs;
+    }
+
+    public MoviePair getBestMoviePairAccordingToFlightDuration(List<MoviePair> moviePairs, int flightDuration) {
+        MoviePair bestMoviePair = new MoviePair(0, 0, 0);
+        moviePairs.stream().forEach(
+                moviePair -> {
+                    if (moviePair.getCombinedDuration() <= flightDuration) {
+                        if (bestMoviePair.getCombinedDuration() < moviePair.getCombinedDuration()) {
+                            bestMoviePair.setMovieOneIndex(moviePair.getMovieOneIndex());
+                            bestMoviePair.setMovieTwoIndex(moviePair.getMovieTwoIndex());
+                            bestMoviePair.setCombinedDuration(moviePair.getCombinedDuration());
+                        }
+                    }
+                }
+        );
+
+        return bestMoviePair;
+
     }
 }
